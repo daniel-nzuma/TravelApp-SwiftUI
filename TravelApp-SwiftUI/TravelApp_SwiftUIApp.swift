a004c0acdc24d2256413144ff539335ddb8b324f
Daniel Nzuma <nzuma.daniel@eclectics.io>
2024-12-13 20:33:42 +0300

    initial commit

---
+++ TravelApp-SwiftUI/TravelApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelApp_SwiftUIApp.swift
+//  TravelApp-SwiftUI
+//
+//  Created by Daniel Nzuma on 13/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct TravelApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
